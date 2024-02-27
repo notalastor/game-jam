74fca54c2751d77685ba87b487215be37b660d9c
alastor <157782660+notalastor@users.noreply.github.com>
2024-02-27 21:40:01 +0300

    edited
    done

---
+++ KinematicBody2D.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var m = Vector2()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		m.x = -300
+	elif Input.is_action_pressed("ui_right"):
+		m.x = 300
+	else:
+		m.x = 0
+	move_and_slide(m,Vector2.UP)
+	
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
